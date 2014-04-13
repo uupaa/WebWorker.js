999b431912ec6e40ae9de9b116f1c1ca2df21906
obara.takao <obara.takao@dena.jp>
2014-04-14 01:36:24 +0900

    remove Valid.js

---
+++ lint/plato/report.js
@@ -0,0 +1,63 @@
+__report = {
+  "reports": [
+    {
+      "info": {
+        "file": "lib/WebWorker.js",
+        "fileShort": "lib/WebWorker.js",
+        "fileSafe": "lib_WebWorker_js",
+        "link": "files/lib_WebWorker_js/index.html"
+      },
+      "jshint": {
+        "messages": 0
+      },
+      "complexity": {
+        "aggregate": {
+          "line": 5,
+          "complexity": {
+            "sloc": {
+              "physical": 257,
+              "logical": 135
+            },
+            "cyclomatic": 51,
+            "halstead": {
+              "operators": {
+                "distinct": 28,
+                "total": 398,
+                "identifiers": [
+                  "__stripped__"
+                ]
+              },
+              "operands": {
+                "distinct": 165,
+                "total": 480,
+                "identifiers": [
+                  "__stripped__"
+                ]
+              },
+              "length": 878,
+              "vocabulary": 193,
+              "difficulty": 40.72727272727273,
+              "volume": 6666.177278721375,
+              "effort": 271495.22007883416,
+              "bugs": 2.222059092907125,
+              "time": 15083.067782157454
+            },
+            "params": 17
+          }
+        },
+        "module": "lib/WebWorker.js",
+        "maintainability": 60.40246799222573
+      }
+    }
+  ],
+  "summary": {
+    "total": {
+      "sloc": 257,
+      "maintainability": 60.40246799222573
+    },
+    "average": {
+      "sloc": 257,
+      "maintainability": "60.40"
+    }
+  }
+}
