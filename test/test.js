24df0f993b896f8fafc27b104f812a0238aa23bb
obara.takao <obara.takao@dena.jp>
2014-04-14 01:18:01 +0900

    add test case

--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
 new Test().add([
         testWebWorker,
+        testWebWorkerError,
+        testWebWorkerInline,
+        testWebWorkerCloseAfterCanNotReuse,
     ]).run(function(err, test) {
-//      if (1) {
+        if (1) {
 //          err || test.worker(function(err, test) {
                 if (!err && typeof WebWorker_ !== "undefined") {
                     var name = Test.swap(WebWorker, WebWorker_);
@@ -11,10 +14,11 @@ new Test().add([
                     });
                 }
 //          });
-//      }
+        }
     });
 
 function testWebWorker(next) {
+
     var task = new Task(4, function(err) {
             if (!err) {
                 console.log("testWebWorker ok");
@@ -74,5 +78,130 @@ function testWebWorker(next) {
     worker1.post({ param: ["foo",   "bar"]    }); // workerID=1,requestID=2
     worker2.post({ param: ["hello", "worker"] }); // workerID=2,requestID=1000
     worker2.post({ param: ["foo",   "bar"]    }); // workerID=2,requestID=1001
+}
+
+
+function testWebWorkerError(next) {
+
+    var task = new Task(1, function(err) {
+            if (!err) {
+                console.log("testWebWorkerError ok");
+                next && next.pass();
+            } else {
+                console.error("testWebWorkerError ng");
+                next && next.miss();
+            }
+        });
+
+    var worker1 = new WebWorker({ source: "./error.js", verbose: true }, function(err, event) {
+            if (err) {
+                task.pass();
+            } else {
+                task.miss();
+            }
+        });
+
+
+    worker1.post({}); // workerID=1,requestID=1
+}
+
+
+function testWebWorkerInline(next) {
+
+    var task = new Task(1, function(err) {
+            if (!err) {
+                console.log("testWebWorkerInline ok");
+                next && next.pass();
+            } else {
+                console.error("testWebWorkerInline ng");
+                next && next.miss();
+            }
+        });
+
+var inlineWorkerSource = _multiline(function() {/*
+
+var _workerOrigin = "";
+
+onmessage = function(event) {
+    // event.data has {
+    //      WORKER_ID:  Integer,
+    //      REQUEST_ID: Integer,
+    //      INIT:       Boolean,
+    //      ORIGIN:     String,
+    //      SCRIPT:     ScriptURLStringArray
+    // }
+
+    var request = event.data;
+    var workerID = request["WORKER_ID"];
+    var requestID = request["REQUEST_ID"];
+
+    if (request["INIT"]) {
+        _workerOrigin = request["ORIGIN"] || "";
+        importScripts.apply(self, request["SCRIPT"]);
+    }
+
+    _do(request);
+
+    function _do(request) {
+        self.postMessage({
+            "WORKER_ID":    workerID,
+            "REQUEST_ID":   requestID,
+            "keys":         "result",
+            "result":       "OK"
+        });
+    }
+};
+
+*/});
+
+    var worker1 = new WebWorker({ inline: true, source: inlineWorkerSource, verbose: true }, function(err, event) {
+            if (!err) {
+                if (event.data.result === "OK") {
+                    task.pass();
+                    return;
+                }
+            }
+            task.miss();
+        });
+
+    worker1.post({}); // workerID=1,requestID=1
+}
+function _multiline(fn) { // @arg Function:
+                          // @ret String:
+    return (fn + "").split("\n").slice(1, -1).join("\n");
+}
+
+
+function testWebWorkerCloseAfterCanNotReuse(next) {
+
+    var task = new Task(2, function(err) {
+            if (!err) {
+                console.log("testWebWorkerCloseAfterCanNotReuse ok");
+                next && next.pass();
+            } else {
+                console.error("testWebWorkerCloseAfterCanNotReuse ng");
+                next && next.miss();
+            }
+        });
+
+    var worker1 = new WebWorker({ source: "./worker.js", verbose: true }, function(err, event) {
+            if (!err) {
+                task.pass();
+
+                worker1.close();
+                try {
+                    worker1.post({ param: ["hello", "worker"] }); // workerID=1,requestID=2
+                    task.miss();
+                } catch (o_o) {
+                    task.pass();
+                }
+
+            } else {
+                task.miss();
+            }
+        });
+
+    worker1.post({ param: ["hello", "worker"] }); // workerID=1,requestID=1
+
 }
 
